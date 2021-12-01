c6735d2c10670e3d032489723448c017c2896600
NotUnlikeTheWaves <hleistra@gmail.com>
2021-12-01 22:39:28 +0100

    Day 1 Part 1

--- 1_1.hs
+++ 1_1.hs
@@ -1,22 +1,22 @@
 -- lookup: 
 -- function composition (can i run function composition directly with arguments)
 
-import Data.Char
+-- import Data.Char
 
 main = do
     contents <- getContents
     -- [10, 18, 17, 20, 15, 23, 24]
     -- [18, 17, 20, 15, 23, 24]
     -- [x   y   n   y   n   y   y]
-    let li = words contents
-    let input = map strToInt li
-    let controlRow = init input
-    let diffRow = tail input
+
+    let contentList = words contents
+    let integerList = map strToInt contentList
+    let controlRow = init integerList
+    let diffRow = tail integerList
     let compute = diff controlRow diffRow
-    print compute
 
     let foundPositive = filter (>0) compute
-    print foundPositive
+    
     print (length foundPositive)
     return ()
 
